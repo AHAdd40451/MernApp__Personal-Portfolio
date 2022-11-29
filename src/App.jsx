af111f9d5481fa2b79d3f72a495b82000cd13691
AHAdd40451 <ahadkhan40451@gmail.com>
2022-11-29 07:07:05 +0500

    changing project

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Experience from "./components/Experience/Experience";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
-import Portfolio from "./components/Portfolio/Portfolio"
+// import Portfolio from "./components/Portfolio/Portfolio"
 import Services from "./components/Services/Services";
 // import Testimonials from "./components/Testimonials/Testimonials";
 import Work from "./components/Work/Work";
